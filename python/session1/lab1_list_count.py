a20d8a35a880c8a97cf63564496748441d26c086
mohamed15aa <mohamed.bakre765@gmail.com>
2026-06-14 14:06:17 +0300

    solve session_1 lap_1

--- python/session1/lab1_list_count.py
+++ python/session1/lab1_list_count.py
@@ -1,13 +1,10 @@
 """Write a Python program to count the number 4 in a given list."""
-
-
 def count(lst):
     """Write your solution here. Don't forget to return the result at the end...."""
     count_4 = 0
     for num in lst:
         if num == 4:
-            count_4 += 1
-            
+            count_4 += 1        
     return count_4  
 
 if __name__ == "__main__":
